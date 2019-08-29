18cfbeb82f2a01707d662c921614004f21ef9884
Shaan <Shaan@admins-MacBook-Pro.local>
2019-08-29 11:25:46 -0700

    added some functions

--- checklist.py
+++ checklist.py
@@ -1,1 +1,21 @@
-print ('hello world')
+checklist = list()
+
+
+
+def create(item):
+    checklist = list()
+    checklist.append('Blue')
+    print(checklist)
+    checklist.append('Orange')
+    print(checklist)
+
+
+def read(index):
+    item = checklist [index]
+
+
+def update(index, item):
+
+
+def destroy(index):
+    pass destroy(index):
