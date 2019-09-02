89b57b7ecf5faf5e1d83fcb11758c23969b2fde2
Shaan <Shaan@admins-MacBook-Pro.local>
2019-09-01 18:27:59 -0700

    Fixes initial errors and prints 4 statements

--- checklist.py
+++ checklist.py
@@ -1,21 +1,41 @@
 checklist = list()
 
 
-
 def create(item):
-    checklist = list()
-    checklist.append('Blue')
-    print(checklist)
-    checklist.append('Orange')
-    print(checklist)
-
+    checklist.append(item)
 
 def read(index):
-    item = checklist [index]
+    return checklist[int(index)]
 
 
 def update(index, item):
+    checklist[index] = item
 
 
 def destroy(index):
-    pass destroy(index):
+    checklist.pop(index)
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(str(index) + list_item)
+        index += 1
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+
+    destroy(1)
+    print(read(0))
+    list_all_items()
+
+
+    # print(read(1))
+
+
+test()
